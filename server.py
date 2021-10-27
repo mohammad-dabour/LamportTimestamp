34e04d0a0571d02541b952894e869ca959f906ba
Mohammad dabour <37874948+mohammad-dabour@users.noreply.github.com>
2021-10-26 23:37:58 -0500

    Update server.py

--- server.py
+++ server.py
@@ -7,27 +7,31 @@ import banking_pb2_grpc
 import multiprocessing
 import datetime
 import time
+import os
+import sys, getopt
+inputfile = ''
+outputfile = 'output.txt'
 
 def get_branches():
-    processes =  json.load(open('input.json','r'))
+    processes =  json.load(open(inputfile,'r'))
     branches = list()
     for p in processes:
-        if p['type'] == 'branch':
+        if p['type'] == 'branch' or p['type'] == 'bank':
             branches.append(p['id'])
      
     return branches
 
 def get_balance(id):
-    processes =  json.load(open('input.json','r'))
+    processes =  json.load(open(inputfile,'r'))
     for p in processes:
-        if p['type'] == 'branch' and p['id'] == id:
+        if (p['type'] == 'branch' or p['type'] == 'bank') and p['id'] == id:
             return p['balance']
 
 def get_ids():
-    processes =  json.load(open('input.json','r'))
+    processes =  json.load(open(inputfile,'r'))
     ids = list()
     for p in processes:
-        if p['type'] == 'branch':
+        if p['type'] == 'branch' or p['type'] == 'bank':
             ids.append(p['id'])
     return ids
 
@@ -41,19 +45,25 @@ def _wait_forever(server):
             time.sleep(_ONE_DAY.total_seconds())
     except KeyboardInterrupt:
         server.stop(None)
+
+
         
 def serve(bind_address, balance, branches):
         
-
+        process = multiprocessing.current_process()
+        print("PIDs:", process.pid)
         #options = (('grpc.so_reuseport', 1),)
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         banking_pb2_grpc.add_BankingServicer_to_server(branch.Branch(id, balance, branches), server)
         server.add_insecure_port(bind_address)
         server.start()
-        _wait_forever(server)
-        #server.wait_for_termination()
+        #_wait_forever(server)
+        server.wait_for_termination()
+
+
 
 def main():
+    
     
     ids = get_ids()
     branches = get_branches()
@@ -62,7 +72,8 @@ def main():
         balance = get_balance(id)
         
         bind_address = '[::]:4080'+str(id)
-        print("create server with id = ", str(id), " and port = 4080",str(id))
+        print("Starting server. Listning on port port  4080"+str(id))
+        
         worker = multiprocessing.Process(target=serve,
                                              args=(bind_address,balance, branches))
         worker.start()
@@ -70,5 +81,36 @@ def main():
     
     for worker in workers:
         worker.join()
+    
 
-main()
+        
+inputfile =''
+outputfile=''
+
+def readargs(argv):
+    global inputfile
+    global outputfile
+  
+    try:
+        
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        if len(opts) < 1:
+            print(len(opts))
+            print ('\nhelp: \n\tcustomer.py -i <inputfile> -o <outputfile>\n')
+            sys.exit(2)    
+    except getopt.GetoptError:
+        
+        print ('customer.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('customer.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+             outputfile = arg
+                
+if __name__ == "__main__":
+    readargs(sys.argv[1:])
+    main()
